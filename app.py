ba4f18ee64fd8dbd730bb09233e2b8437b458d1e
Хорошавцева Анна Георгиевна <KhoroshavtsevaAG@maks.local>
2019-10-04 15:47:12 +0300

    bl! it works! limitations: datetime is not JSON serializable; ascii codec cant encode characters.

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ config.read('snake.ini')
 oracle_login = config['DEFAULT']['login']
 oracle_password = config['DEFAULT']['password']
 
-
 app = Flask(__name__)
 
 #создаем соединение
@@ -18,45 +17,28 @@ dsn_tns = cx_Oracle.makedsn('172.20.0.202', '1521', service_name='mlife1')
 conn = cx_Oracle.connect(user=oracle_login, password=oracle_password, dsn=dsn_tns)
 c = conn.cursor()
 
-
 @app.route("/get_pool_details")
 def get_pool_details():
   pool_id = request.args.get('pool_id')
   calc_date = request.args.get('calc_date')
-
   sql_query = """
-select to_date(sysdate, 'dd/mm/yyyy') bI
-from dual
-
-"""
-
-  def conjure_sql_query(pool_id,calc_date,sql_query = sql_query):
-    valid_sql_query = sql_query.format(
-      pool_id = pool_id,
-      calc_date = calc_date
-      )
-    c.execute(sql_query)
-     # обрабатываем SQL ответ
-    header_list = []
-    for i in c.description:
-        header_list.append(i[0])
-    header_list = tuple(header_list)
-    rows = [row for row in c]
-
-    ###### placeholder!!!
-    header_list = []
-    rows = [[],[]]
-    #return {'header_list' : header_list, 'rows': rows}
+	select 5*5 bI, sysdate werw
+	from dual
+	"""
+  c.execute(sql_query)
+  # обрабатываем SQL ответ
+  header_list = [] #list of objects of any type
+  for i in c.description:
+      header_list.append(i[0])
+  rows = [[],[]]   #may contain of lists as well (apparently)  
+  header_list = tuple(header_list) #unchangeable list
+  rows = [row for row in c] #list
+  #header_list = ["ИД Пула", "Остаток", "Дата","Комментарий"]
+  #rows = [['5','55545','segodnya','bI']]
   
-  end_dict = conjure_sql_query(pool_id,calc_date)
-  header_list = ["ИД Пула", "Остаток", "Дата","Комментарий"]
-  rows = [['5','55545','segodnya','bI']]
   response_dict = {'header_list' : header_list, 'rows': rows}
-
-
-  print(json.dumps(response_dict))
-  return json.dumps(response_dict)
-
+  print (header_list)
+  return json.dumps(response_dict) 
 
 @app.route("/")
 def root():
