a1e838809e3a4ae8ee2cfe198efbd06230961cab
Хорошавцева Анна Георгиевна <KhoroshavtsevaAG@maks.local>
2019-10-17 01:32:01 +0300

    changing output view (tab vs detail) with a param

--- app.py
+++ app.py
@@ -19,7 +19,19 @@ dsn_tns = cx_Oracle.makedsn('172.20.0.202', '1521', service_name='mlife1')
 conn = cx_Oracle.connect(user=oracle_login, password=oracle_password, dsn=dsn_tns, encoding = "UTF-8", nencoding = "UTF-8")
 c = conn.cursor()
 
-def get_pool_details(strat,optdate):
+#функция для привязки названий столбцов к их индексам 
+def fields(cursor):
+  """ Given a DB API 2.0 cursor object that has been executed, returns
+  a dictionary that maps each field name to a column index; 0 and up. """
+  results = {}
+  column = 0
+  for d in cursor.description:
+      results[d[0]] = column
+      column = column + 1
+  return results
+
+#функция для выгрузки инфы по переданным стратегиям и датам в виде таблицы
+def get_pool_table(strat,optdate):
   fd = open('nominal_per_pooolz.sql', 'r')
   sql_query = fd.read()
   fd.close()
@@ -27,7 +39,7 @@ def get_pool_details(strat,optdate):
     strat   = strat,
     optdate = optdate
     )
-  print (valid_sql_query)
+  #print (valid_sql_query)
   #отправляем SQL запрос
   c.execute(valid_sql_query) 
   # обрабатываем SQL ответ
@@ -41,27 +53,51 @@ def get_pool_details(strat,optdate):
        s = s + '<td>' + str(x) + '</td>'  
   s = s + '</tr>' 
   s = s + '</table>' 
-  print('start'+s+'end')
+  #print('start'+s+'end')
   return s 
 
+#функция для выгрузки инфы по переданным стратегиям и датам в виде "карточки пула"
+def get_pool_details(strat,optdate):
+  fd = open('nominal_per_pooolz.sql', 'r')
+  sql_query = fd.read()
+  fd.close()
+  valid_sql_query = sql_query.format(
+    strat   = strat,
+    optdate = optdate
+    )
+  #print (valid_sql_query)
+  #отправляем SQL запрос
+  c.execute(valid_sql_query) 
+  # обрабатываем SQL ответ
+  f = fields(c)
+  s = ''  
+  #записываем Recordset в списки
+  header_list = []
+  rows = [[],[]]
+  for header in c.description:
+    header_list.append(str(header[0]))
+  rows = [row for row in c] #list  
+  #идем собирать карточку из списков
+  for i in range(len(rows)):
+    s = s + '<h2>Карточка пула № ' + str(rows[i][0]) + '</h2><p/>'
+    s = s + 'Стратегия'  + str(rows[i][2]) + '<p/>'
+    s = s + 'Средний КУ' + str(rows[i][3]) + '<p/>'
+    s = s + 'дата пула'  + str(rows[i][4]) + '<p/>'
+    s = s + 'дата пула'  + str(rows[i][5]) + '<p/>'
+  return s
+
+
+#ручка выгружает инфу по актуальному списку пулов 
 @app.route("/apriori")
 def apriori():
+  mode = request.args.get('mode')
+  print (mode)
   fd = open('default_pool_list.sql', 'r')
   sql_query = fd.read()
   fd.close()
   valid_sql_query = sql_query
   #отправляем SQL запрос
-  c.execute(valid_sql_query) 
-  #функция для привязки названий столбцов к их индексам 
-  def fields(cursor):
-    """ Given a DB API 2.0 cursor object that has been executed, returns
-    a dictionary that maps each field name to a column index; 0 and up. """
-    results = {}
-    column = 0
-    for d in cursor.description:
-        results[d[0]] = column
-        column = column + 1
-    return results
+  c.execute(valid_sql_query)  
   #обрабатываем SQL ответ
   f = fields(c)
   #записываем результаты запроса в переменные
@@ -72,8 +108,27 @@ def apriori():
     optdate_list.append(row[f['DATE_OPT']]) 
   strat=','.join(strat_list)
   optdate="to_date('"+"', 'dd.mm.yyyy'), to_date('".join(optdate_list)+"', 'dd.mm.yyyy')"; 
-  return get_pool_details(strat, optdate)
+  if mode=='table':
+    return get_pool_table(strat, optdate)
+  else:	
+    return get_pool_details(strat, optdate)
  
+ 
+#ручка вытаскивает инфу по выбранным пулам
+@app.route("/get_selected_pools")
+def get_selected_pools():
+  print('ya tut')
+  strat = request.args.get('strat')
+  optdate = request.args.get('optdate')
+  mode = request.args.get('mode')
+  print (mode)
+  if mode=='table':
+    return get_pool_table(strat, optdate)
+  else:	
+    return get_pool_details(strat, optdate)
+ 
+ 
+ #ручка вытаскивает актуальный список дат для выбранной стратегии
 @app.route("/get_pool_list")
 def get_pool_list():
   strat   = request.args.get('strat')
@@ -85,27 +140,21 @@ def get_pool_list():
     )
   #отправляем SQL запрос
   c.execute(valid_sql_query) 
-  # обрабатываем SQL ответ
+  #обрабатываем SQL ответ
+  #собираем список чекбоксов с датами
   s = '<h4  style="padding: 0px; margin:0px;margin-bottom:5px;">Даты инвестирования:</h4>'
   s = s+'<input type="button" name="Check_All_opts" value="Снять все" class="chkbtn"	onClick="master_check(\'optdate[]\')" id = "optdate[]"> </br>'  
   for row in c:  
     for x in row:  
        s = s + '<input type="checkbox"  name="optdate[]"   value = "' + str(x) + '" checked > ' + str(x) + ' <Br/>'
-  return s 
+  return s  
 
-@app.route("/get_selected_pools")
-def get_selected_pools():
-  print('ya tut')
-  strat = request.args.get('strat')
-  optdate = request.args.get('optdate')
-  print(strat)
-  print(optdate)
-  return get_pool_details(strat, optdate)
- 
-  
+
+
+
 @app.route("/")
 def root():
   return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run(host="::", port=82, debug = True)
+  app.run(host='0.0.0.0', port=82, debug = True)
