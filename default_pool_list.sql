fb33b1613694fbf9e8c42b4fa549c1477ac0b682
Хорошавцева Анна Георгиевна <KhoroshavtsevaAG@maks.local>
2020-04-01 20:08:52 +0300

    updated query for most recent poolz cuz date_option table not supported by Jurashka anymore

--- default_pool_list.sql
+++ default_pool_list.sql
@@ -1,15 +1,14 @@
-select 
-	p.pool_id pool_id
-  ,	p.strategy_id 	strategy_id
-  , to_char(p.date_option) date_opt
-from lifemakc.date_option t 
-left join life2makc.pool_list p
-  on p.date_option=t.date_opt
-  and p.program_market=t.program
-  and p.term=t.kol_year
+select   distinct
+     inv.pool_id pool_id
+  ,  inv.strategy_id   strategy_id
+  ,  to_char(t.date_option) date_opt
+from lifemakc.dogovor t 
+join life2makc.policy_ku_4_budget_ver_tab v
+     on v.contract_id=t.contract_id
+join lifemakc.cls_invest_details inv
+     on inv.product_key=v.product_key
+     and inv.product_ver = v.product_ver       
 where 1=1 
-and t.inforce=1
-and upper(t.bank) not in 'TST' 
-group by    p.pool_id, 
-  p.strategy_id, p.date_option
+and t.status in (0,1,7,3,6,4)
+and t.begin_date >= (sysdate-30)
 order by 1
