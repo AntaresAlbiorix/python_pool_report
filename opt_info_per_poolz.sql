e2de4d09736a87ae87c13c275779bdc326154a88
KhoroshavtsevaAG <annahorosh93@ya.ru>
2021-06-04 16:12:08 +0300

    options should be picked according to the pool option date, not TS (e.g. option_id 371)

--- opt_info_per_poolz.sql
+++ opt_info_per_poolz.sql
@@ -1,7 +1,7 @@
 select plst.pool_id,
        olst.status,
        --to_char(round(str.coupon, 0)) coupon,        --флаг для особой обработки купонников
-	     olst.isin,
+	   olst.isin,
        to_char(otrs.transaction_date) transaction_date,
        to_char(round(otrs.option_price*100,3)||'%') option_price,
        to_char(nvl(oclc.reporting_date, oclc.valuation_date)) calc_date,
@@ -21,10 +21,10 @@ select plst.pool_id,
                from life2makc.rf_option_calc oclc) oclc
     on oclc.option_id = olst.option_id
    and oclc.rnk = 1
-  left join life2makc.rf_option_settlement os  
+  left join life2makc.rf_option_settlement os
        on os.option_id=olst.option_id   
   --join life2makc.cls_strategy_ref str
   --  on str.strategy_id = olst.strategy_id
   where plst.strategy_id in ({strat})
-   and trim(olst.invest_start_date) in ({optdate}) 
+   and trim(plst.date_option) in ({optdate})
  order by plst.pool_id , otrs.transaction_date
